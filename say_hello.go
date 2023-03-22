56512b809495c2ad9742f59b9196d8dd10ff55e1
Nurdev <nuriswanto66@gmail.com>
2023-03-22 03:48:15 -0700

    update function SayGreeting

--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,17 @@ package go_say_hello
 func SayHello(name string) string {
 	return "Hello " + name
 }
+
+func SayGreeting(country string) string {
+	if country == "Indonesia" {
+		return "Selamat Pagi"
+	} else if country == "Japan" {
+		return "Konichiwa"
+	} else if country == "France" {
+		return "Bonjour"
+	} else if country == "Saudi Arabia" {
+		return "Sobahun Nur"
+	} else {
+		return "Good Morning"
+	}
+}
